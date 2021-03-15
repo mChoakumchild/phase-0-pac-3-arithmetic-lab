cd0de2e57cc333e68adb15f775628ef685b0a5ce
Elijah Schiltz-Rouse <elijahforest101@gmail.com>
2021-03-14 21:15:17 -0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(num1,num2) {
+const sum = num1 + num2;
+return sum;
+}
+
+function subtract(num1, num2){
+const sub = num1 - num2;
+return sub;
+}
+
+function divide(num1, num2){
+const div = num1 / num2;
+return div;
+}
+
+function multiply(num1, num2){
+const mul = num1 * num2;
+return mul;
+}
+
+function increment(n) {
+const inc = ++n;
+return inc;
+
+}
+
+function decrement(n) {
+const inc = --n;
+return inc;
+    
+}
+
+function makeInt(k) {
+return parseInt(k, 10);
+
+}
+function preserveDecimal(k) {
+return parseFloat(k,10);
+}
+
+
